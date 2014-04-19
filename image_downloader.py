da5f39b78e4784cc70d5da7badd95239c3cac5be
Alexander Rowland <alexander.c.rowland@gmail.com>
2014-04-19 17:21:48 -0400

    Got file_parser() working properly. Other functions likely broken

--- image_downloader.py
+++ image_downloader.py
@@ -6,34 +6,16 @@ import imghdr
 import httplib
 import cStringIO
 
-# Need parser that searches txt file for addresses beginning with http://i.groupme.com/
-
-group_id = "transcript-5815609.txt"
-transcript = open(group_id)
-for line in transcript:
-    pass
-#    # Do string processing here
-#    #if 'http://i.groupme.com/' in 
-#    #re.search(pattern, string, flags=0)
-#    match = re.search('http://i.groupme.com/', line, flags=0)
-#    if match:
-#        process(match)
-    
-#transcript.close()
-
-#handlers= {"p": # p tag handler
-#           "h1": # h1 tag handler
-#          }
-
-## ... in the loop
-#    if lyne.rstrip() in handlers :  # strip to remove trailing whitespace
-#       # close current handler?
-#        # start new handler?
-#    else :
-        # pass string to current handler
-
 example_domain = 'www.ovguide.com'
 example_path = '/img/global/ovg_logo.png'
+
+## Checks if new image directory exists, creates one if it doesn't
+#  @param N/a
+#  @return N/a
+def directory_exists():
+    if not os.path.exists(script_directory + '/images/'):
+        os.makedirs(script_directory + '/images/')
+
 
 ## Checks url to see if it's an image.
 #  @param domain_url http domain of a potential image's url
@@ -52,16 +34,27 @@ def is_image(domain_url, path_url):
         is_image_bool = True
     return is_image_bool
 
+
+## Parses a transcript file for urls.
+#  @param transcript_file Name of transcript file to be parsed
+#  @return match String of matched url
+def file_parser(transcript_file):
+    for line in transcript_file:
+        # Do string processing here
+        match = re.search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+        if match != None:
+            #if is_image(match):
+            print(match.group(0))
+
+file_name = "transcript-5815609.txt"
+transcript = open(file_name, "r")
 image_id = 'e3f9d0c0ac520130022766900689df55'
 url = 'http://i.groupme.com/' + image_id
 image = urllib.URLopener()
 script_directory = os.path.dirname(os.path.abspath(__file__))
 
-## Checks if new image directory exists, creates one if it doesn't
-# 
-if not os.path.exists(script_directory + '/images/'):
-    os.makedirs(script_directory + '/images/')
 
-image.retrieve(url, script_directory + '/images/' + image_id + '.jpg')
+file_parser(transcript)
+transcript.close()
+#image.retrieve(url, script_directory + '/images/' + image_id + '.jpg')
 
-# (?P<url>http?://[^\s]+)
