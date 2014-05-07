321895182e30c025b1889a6033f9dfb6b2c7a21c
Alexander Rowland <alexander.c.rowland@gmail.com>
2014-05-07 02:06:04 -0400

    Added try-catch for potential NoneType exception for regular expression search

--- image_downloader.py
+++ image_downloader.py
@@ -14,7 +14,11 @@ from socket import timeout
 def file_parser(file_name):
     for line in file_name:
         match = search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
-        current_match = match.group(0)
+        try:
+            current_match = match.group(0)
+        except AttributeError:
+            continue
+
         if match is not None:
             try:
                 if is_image(current_match):
