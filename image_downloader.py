b3a85ed69b09e1e9c979c8afb8c5680e74a64da6
Antony Cherepanov <antony.cherepanov@gmail.com>
2014-07-22 19:29:45 +0600

    Image downloading process checked

--- image_downloader.py
+++ image_downloader.py
@@ -1,90 +1,10 @@
-from sys import exit, argv
-from os import makedirs
-from os.path import dirname, exists, abspath, isabs, isdir
-from re import search
-from urllib.error import HTTPError, URLError, ContentTooShortError
+import sys
+import os
+import re
+from urllib.error import HTTPError, URLError
 from urllib.request import urlopen, urlretrieve
-from imghdr import what
+import imghdr
 from socket import timeout
-
-
-## Parses a file for urls.
-#  @param: file_name - name of file to be parsed
-#  @return: list of valid URLs
-def file_parser(file_name):
-    valid_urls = list()
-    for line in file_name:
-        match = search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
-        try:
-            current_match = match.group(0)
-        except AttributeError:
-            continue
-
-        if match is not None:
-            try:
-                if is_image(current_match):
-                    valid_urls.append(current_match)
-            except HTTPError as err:
-                print(current_match + '\n' + "Bad URL or timeout: {0}".format(err))
-                continue
-            except URLError as err:
-                print(current_match + '\n' + "Bad URL or timeout: {0}".format(err))
-                continue
-            except timeout as err:
-                print(current_match + '\n' + "Timeout error: {0}".format(err))
-                continue
-
-    return valid_urls
-
-
-## Checks url to see if it's an image.
-#  @param: url - Url of a potential image
-#  @return: boolean value for whether url contains an image
-def is_image(url):
-    url_data = urlopen(url, timeout=60).read()
-    what_type = what('ignore', url_data)
-
-    is_image_bool = False
-    if what_type is not None:
-        is_image_bool = True
-
-    return is_image_bool
-
-
-## Checks if new image directory exists, creates one if it doesn't.
-#  @param: t_path - string with path to folder for images
-#  @return: boolean value for whether path to folder is OK
-def check_dir(t_path):
-    if not isabs(t_path):
-        return False
-    if not isdir(t_path):
-        makedirs(t_path)
-
-    return True
-
-
-## Downloads images to folder.
-#  @param: t_urls - list of url of image to be downloaded
-#  @param: t_folder - folder for images
-def img_downloader(t_urls, t_folder):
-    images_per_percent = len(t_urls) / 100
-    counter = 0
-    percent = 0
-
-    for url in t_urls:
-        counter += 1
-        path = t_folder + url.rsplit('/', 1)[1]
-        try:
-            urlretrieve(url, path)
-        except URLError as err:
-            print("Failed to download: " + url)
-            print(err.reason)
-            continue
-
-        if counter == images_per_percent:
-            counter = 0
-            percent += 1
-            print(percent + "%")
 
 
 ## Opens a text file.
@@ -100,27 +20,105 @@ def open_text_file(t_file_path):
     return transcript
 
 
+## Parses a file for urls.
+#  @param: t_file - file object to be parsed
+#  @return: list of valid URLs
+def file_parser(t_file):
+    valid_urls = list()
+    for line in t_file:
+        match = re.search('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', line)
+        try:
+            current_match = match.group(0)
+        except AttributeError:
+            continue
+
+        if match is None:
+            continue
+
+        try:
+            check_result = is_image(current_match)
+        except HTTPError as err:
+            print(current_match + '\n' + "Bad URL or timeout: {0}".format(err))
+            continue
+        except URLError as err:
+            print(current_match + '\n' + "Bad URL or timeout: {0}".format(err))
+            continue
+        except timeout as err:
+            print(current_match + '\n' + "Timeout error: {0}".format(err))
+            continue
+
+        if check_result is True:
+            valid_urls.append(current_match)
+
+    return valid_urls
+
+
+## Checks url to see if it's an image.
+#  @param: t_url - Url of a potential image
+#  @return: boolean value for whether url contains an image
+def is_image(t_url):
+    try:
+        url_data = urlopen(t_url, timeout=60).read()
+    except URLError as err:
+        return False
+
+    what_type = imghdr.what("filename to ignore", url_data)
+    if what_type is None:
+        return False
+
+    return True
+
+
+## Checks if new image directory exists, creates one if it doesn't.
+#  @param: t_path - string with path to folder for images
+#  @return: boolean value for whether path to folder is OK
+def check_dir(t_path):
+    if not os.path.isabs(t_path):
+        return False
+    if not os.path.isdir(t_path):
+        os.makedirs(t_path)
+
+    return True
+
+
+## Downloads images to folder.
+#  @param: t_urls - list of url of image to be downloaded
+#  @param: t_folder - folder for images
+def img_downloader(t_urls, t_folder):
+    for url in t_urls:
+        print(url)
+        path = os.path.join(t_folder, url.rsplit('/', 1)[1])
+        try:
+            urlretrieve(url, path)
+        except URLError as err:
+            print("Failed to download: " + url)
+            print(err.reason)
+            continue
+
+
 ## Main method
 def start():
-    if len(argv) != 2:
+    if len(sys.argv) != 3:
         print("Invalid arguments")
-        exit()
+        sys.exit()
 
-    file = open_text_file(argv[0])
+    print("Checking...")
+    file = open_text_file(sys.argv[1])
     if file is None:
-        exit()
+        sys.exit()
 
     valid_urls = file_parser(file)
     file.close()
     if len(valid_urls) <= 0:
         print("Nothing to download")
-        exit()
+        sys.exit()
 
-    folder_path = argv[1]
+    folder_path = sys.argv[2]
     if not check_dir(folder_path):
         print("Failed to create folder for images")
-        exit()
+        sys.exit()
 
+    print("Downloading...")
     img_downloader(valid_urls, folder_path)
     print("Done.")
 
