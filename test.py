c94544c36f1f9adff4ded0744cdfa92194ba88b7
Antony Cherepanov <antony.cherepanov@gmail.com>
2014-07-26 17:59:06 +0600

    Tests for open_text_file()

--- test.py
+++ test.py
@@ -33,11 +33,47 @@ def test_check_dir_invalid_path():
     fake_folder_path = "/fake/folder"
     assert(not image_downloader.check_dir(fake_folder_path))
 
+    fake_folder = "../fake/folder"
+    assert(not image_downloader.check_dir(fake_folder))
+
+
+def test_open_file_invalid():
+    fake_file = "/fake/file.txt"
+    assert(not image_downloader.open_text_file(fake_file))
+
+
+def test_open_file_valid():
+    file_path = create_empty_file()
+    assert(image_downloader.open_text_file(file_path))
+    remove_file(file_path)
+
+
+def create_empty_file():
+    path = os.getcwd() + "/test_file.txt"
+    open(path, 'a').close()
+    return path
+
+
+def create_file_with_urls():
+    path = os.getcwd() + "/test_file.txt"
+    with open(path, 'a') as file:
+        file.write("http://upload.wikimedia.org/wikipedia/commons/f/ff/Picea_likiangensis.jpg")
+        file.write("http://www.bountiful-farms.com/photos/plants/piceafastigiata.jpg")
+        file.write("http://farm1.static.flickr.com/67/175175908_9d19e087c7.jpg")
+
+    return path
+
+
+def remove_file(t_path):
+    os.remove(t_path)
+
 
 if __name__ == '__main__':
     test_is_image_valid_url()
     test_is_image_invalid_url()
     test_check_dir_valid_path()
     test_check_dir_invalid_path()
+    test_open_file_invalid()
+    test_open_file_valid()
 
     print("Tests passed!")
