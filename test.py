33a0c00ccc0d6242072c9191379c60960efbb7b6
Antony Cherepanov <antony.cherepanov@gmail.com>
2014-07-26 15:46:46 +0600

    Tests for function is_image()

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
 __author__ = "Antony Cherepanov"
 
+import image_downloader
+
+
+def test_is_image_valid_url():
+    valid_url = "http://upload.wikimedia.org/wikipedia/commons/f/ff/Picea_likiangensis.jpg"
+    assert(image_downloader.is_image(valid_url))
+
+
+def test_is_image_invalid_url():
+    invalid_url = "http://upload.wikimedia.org/wikipedia/commons/f/ff/Picea_likiangensis.txt"
+    assert(not image_downloader.is_image(invalid_url))
+
+
 if __name__ == '__main__':
-    print("Test passed!")
+    test_is_image_valid_url()
+    print("Tests passed!")
