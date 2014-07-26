3f542605a7eb2464e36edba7189dcf001fbb5b3c
Antony Cherepanov <antony.cherepanov@gmail.com>
2014-07-26 20:07:06 +0600

    Tests for img_downloader()

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ __author__ = "Antony Cherepanov"
 
 import image_downloader
 import os
+import shutil
 
 
 def test_is_image_valid_url():
@@ -15,18 +16,14 @@ def test_is_image_invalid_url():
 
 
 def test_check_dir_valid_path():
-    path = os.getcwd() + "/test_folder"
-
-    # Check if such folder exist
-    if not os.path.exists(path):
-        os.makedirs(path)
-
+    # Check with existent folder
+    path = get_path_to_folder_for_images()
     assert(image_downloader.check_dir(path))
 
     # Check with non existent folder
-    os.rmdir(path)
+    remove_folder(path)
     assert(image_downloader.check_dir(path))
-    os.rmdir(path)
+    remove_folder(path)
 
 
 def test_check_dir_invalid_path():
@@ -51,43 +48,103 @@ def test_open_file_valid():
 def test_file_parsing():
     file_path = create_file_with_urls()
     file = open(file_path, 'r')
+    num_of_urls = sum(1 for line in file)
+    file.seek(0, 0)
     valid_urls = image_downloader.file_parser(file)
 
     file.close()
     remove_file(file_path)
 
-    if len(valid_urls) != 3:
+    if len(valid_urls) != num_of_urls:
         raise Exception("Fail. Not all URLs validated")
 
 
+def test_img_downloader_valid_urls():
+    urls = get_test_urls()
+    folder_path = get_path_to_folder_for_images()
+    image_downloader.img_downloader(urls, folder_path)
+
+    files = os.listdir(folder_path)
+    remove_folder(folder_path)
+    if len(files) != len(urls):
+        raise Exception("Fail. Not all images downloaded")
+
+
+def test_img_downloader_invalid_urls():
+    urls = get_invalid_urls()
+    folder_path = get_path_to_folder_for_images()
+    image_downloader.img_downloader(urls, folder_path)
+
+    files = os.listdir(folder_path)
+    remove_folder(folder_path)
+    if len(files) != 0:
+        raise Exception("Fail. Image downloaded by invalid link")
+
+
 def create_empty_file():
-    path = os.getcwd() + "/test_file.txt"
+    path = get_test_file_path()
     open(path, 'a').close()
     return path
 
 
+def get_test_file_path():
+    return os.getcwd() + "/test_file.txt"
+
+
 def create_file_with_urls():
-    path = os.getcwd() + "/test_file.txt"
+    path = get_test_file_path()
+    urls = get_test_urls()
     with open(path, 'a') as file:
-        file.write("http://upload.wikimedia.org/wikipedia/commons/f/ff/Picea_likiangensis.jpg" + "\n")
-        file.write("http://www.bountiful-farms.com/photos/plants/piceafastigiata.jpg" + "\n")
-        file.write("http://farm1.static.flickr.com/67/175175908_9d19e087c7.jpg" + "\n")
+        for url in urls:
+            file.write(url + "\n")
         file.close()
 
     return path
+
+
+def get_test_urls():
+    urls = list()
+    urls.append("http://upload.wikimedia.org/wikipedia/commons/f/ff/Picea_likiangensis.jpg")
+    urls.append("http://farm1.static.flickr.com/67/175175908_9d19e087c7.jpg")
+    urls.append("http://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/150px-Wikipedia-logo-v2.svg.png")
+    return urls
+
+
+def get_invalid_urls():
+    urls = list()
+    urls.append("http://www.bountiful-farms.com/photos/plants/piceafastigiata.jpg")
+    return urls
 
 
 def remove_file(t_path):
     os.remove(t_path)
 
 
+def get_path_to_folder_for_images():
+    path = os.getcwd() + "/test_folder"
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+    return path
+
+
+def remove_folder(t_path):
+    shutil.rmtree(t_path, True)
+
+
 if __name__ == '__main__':
     test_is_image_valid_url()
     test_is_image_invalid_url()
+
     test_check_dir_valid_path()
     test_check_dir_invalid_path()
-    test_open_file_invalid()
+
     test_open_file_valid()
+    test_open_file_invalid()
+
     test_file_parsing()
+
+    test_img_downloader_valid_urls()
+    test_img_downloader_invalid_urls()
 
     print("Tests passed!")
